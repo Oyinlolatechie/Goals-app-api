a00365629f6e962b61c8ba4604538ed769bbd74a
Oyinlolatechie <Adepitanoyinlola4@gmail.com>
2022-12-03 09:00:03 +0100

    implemented signin, generated jwt-token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,8 @@
 const asyncHandler = require('express-async-handler')
 const userModel = require('../models/userModel')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 
 //@desc sign up
 //@route POST /users
@@ -11,7 +13,8 @@ exports.signUp = asyncHandler (async (req, res) => {
 
     //verify user' input
     if(!email || !username || !password) {
-        res.status(400).json("please enter all fields")
+        res.status(400)
+        throw new Error("please enter all fields")
     }
 
     //confirm if user already exist
@@ -39,10 +42,12 @@ exports.signUp = asyncHandler (async (req, res) => {
     } 
 
     res.status(200).json({
-        firstname : req.body.firstname,
-        lastname : req.body.lastname,
-        username : req.body.username,
-        email : req.body.email
+        _id : user.id,
+        firstname : user.firstname,
+        lastname : user.lastname,
+        username : user.username,
+        email : user.email,
+        token : generateToken(user.id)
     })
 })
 
@@ -50,9 +55,34 @@ exports.signUp = asyncHandler (async (req, res) => {
 //@route POST /users/login
 //@access Public
 exports.signIn = asyncHandler ( async (req, res) => {
-    res.json({
-        message : "userLogin"
-    })
+    const { email, password } = req.body
+
+      //verify user' input
+    if(!email || !password) {
+        res.status(400)
+        throw new Error("please enter all fields")
+    }
+
+    //check if email exist 
+    const user = await userModel.findOne({email})
+
+    // res.status(200).json(user)
+
+      //check password 
+      if(user && (await bcrypt.compare(password, user.password))) {
+       
+        res.status(200).json({
+            _id : user.id,
+            firstname : user.firstname,
+            lastname : user.lastname,
+            username : user.username,
+            email : user.email,
+            token : generateToken(user._id)
+        })
+    } else{
+        res.status(400)
+        throw new Error("user not found") 
+    }
 })
 
 
@@ -65,3 +95,6 @@ exports.getUser = asyncHandler (async (req, res) => {
     })
 })
 
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {expiresIn :'30d'})
+}
